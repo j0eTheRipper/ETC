0761ce92533f748dedc053abff63c01c817e8fcd
alial <sultan.shi1212@gmail.com>
2023-11-07 12:57:31 +0800

    finished adding tutor part.

---
+++ MainSystemMenus.py
@@ -0,0 +1,36 @@
+
+
+
+def loginMenu():
+    print("*********************************************************")
+    print("****                  Login Page                     ****")
+
+
+def AdminMenu():
+    print("*********************************************************")
+    print("****                 Admin Menu                      ****")
+    print("1- Tutor Management")
+    print("2- Reception Management")
+
+def TutorManagementMenu():
+    print("*********************************************************")
+    print("****                 Tutor Menu                      ****")
+    print("1- Register New Tutor")
+    print("2- Delete Tutor")
+
+def ReceptionManagementMenu():
+    print("*********************************************************")
+    print("1- Register New Reception")
+    print("2- Delete Reception")
+
+def ProfileManagementMenu():
+    print("*********************************************************")
+    print("1- Update own profile ")
+
+
+
+def IncomeManagementMenu():
+    print("*********************************************************")
+    print("1-level")
+    print("2-subject")
+
